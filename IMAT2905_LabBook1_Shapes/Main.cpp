24110cee95fc49a03bf8a6252ac806b64975ab4b
kaneg <kanegarrett@hotmail.com>
2021-10-25 11:34:59 +0100

    Add project files.

---
+++ IMAT2905_LabBook1_Shapes/Main.cpp
@@ -0,0 +1,49 @@
+#include<SFML/Graphics.hpp>
+
+int main()
+{
+    sf::RenderWindow window(sf::VideoMode(800, 600), "SFML works!");
+    //sf::CircleShape shape(100.f);
+    //shape.setFillColor(sf::Color::Green);
+    sf::VertexArray rectang(sf::Quads, 4);
+    //sf::VertexArray triangle(sf::Triangles, 3);
+    //sf::RectangleShape rectang;
+    //sf::Vector2f(rect)
+
+    //// define the position of the quad's points
+     //= sf::Vector2f(10, 10);
+    rectang[1].position = sf::Vector2f(100, 10);
+    rectang[2].position = sf::Vector2f(100, 100);
+    rectang[3].position = sf::Vector2f(10, 100);
+    // define the color of the triangle's points
+    //rectang[0].color = sf::Color::Red;
+    //rectang[1].color = sf::Color::Blue;
+    //rectang[2].color = sf::Color::Green;
+    rectang[3].color = sf::Color::Yellow;
+
+   
+
+    float xVelocity = {1};
+    float yVelocity = {1};
+
+  
+
+    while (window.isOpen())
+    {
+        sf::Event event;
+        while (window.pollEvent(event))
+        {
+            if (event.type == sf::Event::Closed)window.close();
+            if (sf::Keyboard::isKeyPressed(sf::Keyboard::Escape))window.close();
+        }
+
+        rectang[0].position.x += xVelocity;
+        rectang[2].position.x += yVelocity;
+        window.clear();
+        window.draw(rectang);
+        //window.draw(triangle);
+        window.display();
+    }
+
+    return 0;
+}
